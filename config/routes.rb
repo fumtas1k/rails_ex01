46b5a1bd463815472f0f8c69aa583004140dceee
fumtas1k <fumta01a7@gmail.com>
2022-01-04 22:32:04 +0900

    完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'contacts/new'
-  get 'contacts/create'
+  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
